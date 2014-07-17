c6ef115099cd395f58d2ee7009e4bb6dc0834557
pavankumar77 <pavankumar.s@hashedin.com>
2014-07-17 14:57:17 +0530

    read data from git and will be working save in db

---
+++ src/main/java/com/hashedin/devd/integration/CommitedAt.java
@@ -0,0 +1,74 @@
+package com.hashedin.devd.integration;
+
+import com.hashedin.devd.realdata.JavaUrlConnectionReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TimeZone;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CommitedAt {
+
+	public int lastCommitedAt() {
+		JavaUrlConnectionReader realData = new JavaUrlConnectionReader();
+
+		String output = realData.getUrlContents("url");
+
+		List<String> createdData = createdAt("output");
+
+		int l = createdData.size();
+
+		String timeStamp = createdData.get(l);
+
+		l = differenceCalculator(timeStamp);
+
+		return l;
+	}
+
+	public int differenceCalculator(String s) {
+
+		TimeZone tz = TimeZone.getTimeZone("UTC");
+
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+
+		df.setTimeZone(tz);
+
+		String currentDate = df.format(new Date());
+	
+		
+		return 0;
+	}
+
+	public List<String> createdAt(String output) {
+
+		List<String> list = new ArrayList<String>();
+
+		JSONArray jArray;
+
+		try {
+			jArray = new JSONArray(output);
+
+			for (int i = 0; i < jArray.length(); i++) {
+
+				JSONObject jsonObj = jArray.getJSONObject(i);
+
+				String type = (String) jsonObj.get("type");
+
+				if (type.endsWith("PushEvent")) {
+					String createdAt = (String) jsonObj.get("created_at");
+					// System.out.println(createdAt);
+					list.add(createdAt);
+				}
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+}
